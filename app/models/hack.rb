ed06519bb80832f317b01ef7855260a37cdd7316
kimberly peng <kimmypeng@gmail.com>
2016-05-27 22:49:40 -0400

    creating variable name in hack.rb image validation, adding lifehacks to hacks#index as well as logout button

--- app/models/hack.rb
+++ app/models/hack.rb
@@ -5,10 +5,10 @@ class Hack < ActiveRecord::Base
 	has_many :tags, through: :hack_tags
 
 	# validates image attachment
-	has_attached_file :avatar, :styles =>
+	has_attached_file :lhimg, :styles =>
 	{ :medium => "300x300>", :thumb => "100x100>" },
 	:default_url => "/images/:style/missing.png"
-	validates_attachment_content_type :avatar,
+	validates_attachment_content_type :lhimg,
 	:content_type => /\Aimage\/.*\Z/
 
 end
