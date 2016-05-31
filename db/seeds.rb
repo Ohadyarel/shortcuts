5592d5d557d79d4a8b6fa8041839d746745808cc
Jerry Bi <jerrybi0522@gmail.com>
2016-05-31 10:24:09 -0400

    Adding seeds for users hacks and tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: "Finn101", password: "hocuscrocus", email: "email@email1.com")
+User.create(username: "Cuco", password: "kingofplotbunnies", email: "email@email2.com")
+User.create(username: "IlikeTurtles", password: "somethingrandom", email: "email@email3.com")
+
+Hack.create(user_id: 1, title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
+Hack.create(user_id: 2, title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
+Hack.create(user_id: 3, title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
+
+Tag.create(category: "home")
+Tag.create(category: "work")
+Tag.create(category: "food")
