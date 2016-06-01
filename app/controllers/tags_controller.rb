67cabe347ab80a280c8f75768a4f58e27ad364be
kimberly peng <kimmypeng@gmail.com>
2016-06-01 18:51:30 -0400

    adding a show for tag to be populated

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,8 @@
 class TagsController < ApplicationController
+	# show the lifehacks based on the tag search
+	def show
+		puts "WHAT AM I GETTING?"
+		params.inspect
+		# @hacks=Tag.where(category:params)
+	end
 end
